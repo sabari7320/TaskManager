0d21befc78b9a4cac25a3e87652f9b68d5936600
sabari <sabarinathan000777@gmail.com>
2025-11-24 11:27:40 +0530

    comment  removed and host url fixed

--- docs/docs.go
+++ docs/docs.go
@@ -376,7 +376,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "taskmanager-ekqd.onrender.com   // change to your actual render URL",
+	Host:             "taskmanager-ekqd.onrender.com",
 	BasePath:         "/",
 	Schemes:          []string{"https"},
 	Title:            "Task Manager API",
